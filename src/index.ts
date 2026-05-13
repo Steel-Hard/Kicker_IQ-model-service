a5467ba848f7abd056def7f1dc022485cd54cf4f
claudsaints <claudiodsantoss7@gmail.com>
2026-05-12 21:46:37 -0300

    fix: ajuste para integração

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import app from "./app"
+import dotenv from "dotenv"
 
-const port = 3000
+const port = process.env.PORT || 3001
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
     console.log(`✅ | Servidor rodando na porta http://0.0.0.0:${port}`)
 })
